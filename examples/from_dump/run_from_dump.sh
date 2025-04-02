3e2498fea4fc845aee934716049a150485acb0e6
Hagai Masaya <hagai.masaya.v9@s.mail.nagoya-u.ac.jp>
2025-04-02 23:28:15 +0900

    make example

---
+++ examples/from_dump/run_from_dump.sh
@@ -0,0 +1,5 @@
+tg16.py anthracene_s1_freq.gjf
+tg16.py anthracene_t1_freq.gjf
+pysocDeriv -c config_T1_S0.yaml
+pysocDeriv -c config_T1_S1.yaml
+
---
+++ examples/from_log/run_from_log.sh
@@ -0,0 +1,4 @@
+g16 anthracene_s1_freq.gjf
+g16 anthracene_t1_freq.gjf
+pysocDeriv -c config_T1_S0.yaml
+pysocDeriv -c config_T1_S1.yaml
